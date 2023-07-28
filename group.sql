68b1670544546ec99fc5b29a405faa91b52891ac
patrictobola <tobola92@gmail.com>
2023-07-28 17:51:36 +0200

    group 3

--- group.sql
+++ group.sql
@@ -7,4 +7,7 @@ SELECT COUNT(`id`) AS `students`, YEAR(`enrolment_date`) AS `anno_iscrizione` FR
 SELECT COUNT(`id`) AS `teachers`, `office_address` AS `office` FROM `teachers` GROUP BY `office`; 
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+
+SELECT COUNT(`exam_id`), AVG(`vote`) FROM `exam_student` GROUP BY `exam_id`; 
+
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento

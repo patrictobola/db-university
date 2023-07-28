fc5a8060a1c178697de5c6306125d6c77c83b7fe
patrictobola <tobola92@gmail.com>
2023-07-28 16:36:42 +0200

    5

--- select.sql
+++ select.sql
@@ -11,6 +11,9 @@ SELECT * FROM `courses` WHERE `cfu` > 10;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+
+SELECT * FROM `exams` WHERE HOUR(`hour`) >= 14 AND `date` LIKE '2020-06-20'; 
+
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
 SELECT * FROM `degrees` WHERE `level` LIKE 'magistrale%'; 

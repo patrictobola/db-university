c6af4ad020f5f8fedde5fe34218819de56b0873c
patrictobola <tobola92@gmail.com>
2023-07-28 16:29:46 +0200

    7

--- select.sql
+++ select.sql
@@ -13,6 +13,9 @@ SELECT * FROM `courses` WHERE `cfu` > 10;
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 -- 7. Da quanti dipartimenti è composta l'università? (12)
+
+SELECT COUNT(*) AS `total_departments` FROM `departments`; 
+
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
 SELECT * FROM `teachers` WHERE `phone` IS NULL; 

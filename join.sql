0ea92fa412626314b373dbc75aa862c5164bfee8
patrictobola <tobola92@gmail.com>
2023-07-31 14:22:05 +0200

    join 3

--- join.sql
+++ join.sql
@@ -14,7 +14,11 @@ WHERE DEP.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
-
+SELECT T.`id`, T.`name`, T.`surname`, C.`name` AS 'course_name'
+FROM `teachers` AS T
+JOIN `course_teacher` AS CT ON T.`id` = CT.`teacher_id`
+JOIN `courses` AS C ON C.`id` = CT.`course_id`
+WHERE T.`id` = 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
